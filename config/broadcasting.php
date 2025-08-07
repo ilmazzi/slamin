9ac518a3afcb35ae88b5575576e9e3226f190a9f
ilmazzi <davide.mazzitelli@yahoo.it>
2025-08-07 11:06:03 +0200

    update

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -63,10 +63,7 @@ return [
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
             ],
-            'curl_options' => [
-            CURLOPT_SSL_VERIFYHOST => 0,
-            CURLOPT_SSL_VERIFYPEER => 0,
-        ],
+            
         ],
 
         'ably' => [
