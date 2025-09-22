4d04e3221bed13d10b04fd6b28dc37be2bf634f2
ilmazzi <davide.mazzitelli@yahoo.it>
2025-09-22 17:22:27 +0200

    update

--- resources/views/media/index.blade.php
+++ resources/views/media/index.blade.php
@@ -2,6 +2,35 @@
 
 @section('title', __('common.media_section') . ' - Slamin')
 
+@section('css')
+<style>
+/* Assicura che il footer sia sempre visibile */
+footer {
+    position: fixed !important;
+    bottom: 0 !important;
+    left: 0 !important;
+    width: 100% !important;
+    z-index: 1000 !important;
+    display: block !important;
+    visibility: visible !important;
+    opacity: 1 !important;
+}
+
+/* Assicura che il footer non si sovrapponga alla sidebar */
+@media (min-width: 1200px) {
+    footer {
+        padding-left: 20rem !important;
+    }
+}
+
+@media (max-width: 1199px) {
+    footer {
+        padding-left: 4.5rem !important;
+    }
+}
+</style>
+@endsection
+
 @section('main-content')
 <div class="container-fluid">
     <!-- Breadcrumb -->
@@ -11,7 +40,7 @@
                 <i class="ph ph-video-camera me-2"></i>
                 {{ __('common.media_section') }}
             </h4>
-            
+
         </div>
     </div>
 
