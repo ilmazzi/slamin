32f8c11dee46eac1ea3220c58d4513c2ea747cd6
ilmazzi <davide.mazzitelli@yahoo.it>
2025-10-14 22:35:13 +0200

    Fix Wirechat - registra WirechatBridgeServiceProvider nel bootstrap/providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    App\Providers\WirechatBridgeServiceProvider::class,
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
     App\Providers\BroadcastServiceProvider::class,
