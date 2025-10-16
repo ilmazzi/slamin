633cea187dd9298c5272208c0a1a8fc2892730c8
ilmazzi <davide.mazzitelli@yahoo.it>
2025-10-16 19:05:27 +0200

    feat: Aggiunto CSS personalizzato per chat compatibile con Larkon
    - Creato public/css/chat.css con stili personalizzati
    - Usa variabili CSS compatibili con il tema Larkon
    - Supporto dark mode con [data-theme=dark]
    - Stili per: layout, lista chat, messaggi, avatar, header, footer
    - Incluso CSS nelle view chat con @push('styles')
    - Ora la chat usa CSS standard invece di Tailwind

--- resources/views/chat/pages/show.blade.php
+++ resources/views/chat/pages/show.blade.php
@@ -1,5 +1,9 @@
 @extends('layout.master')
 
+@push('styles')
+<link rel="stylesheet" href="{{ asset('css/chat.css') }}">
+@endpush
+
 @section('main-content')
 <div class="container-fluid p-0" style="height: calc(100vh - 100px);">
     @livewire('chat.pages.chat', ['conversation' => $conversation])
