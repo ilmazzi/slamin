82c2c3430162aa28db66cafe60c882f92e4fcab2
ilmazzi <davide.mazzitelli@yahoo.it>
2025-10-21 20:42:33 +0200

    Semplificato JavaScript snap - risolti errori Alpine.js
    - Rimosso JavaScript complesso da x-data
    - Click sui marker ora usa JavaScript diretto inline
    - Eliminati errori di sintassi Alpine.js
    - Sistema snap funzionante e stabile
    - Nessun errore di parsing JavaScript

--- resources/views/livewire/snap/snap-timeline.blade.php
+++ resources/views/livewire/snap/snap-timeline.blade.php
@@ -1,24 +1,7 @@
 <div x-data="{ 
     currentTime: $wire.entangle('currentTime'),
-    duration: {{ $duration }},
-    
-    initTimeline() {
-        // Listener per aggiornamenti tempo
-        Livewire.on('video-time-update', (data) => {
-            this.currentTime = data.time;
-        });
-    },
-    
-    seekToTimestamp(timestamp) {
-        // Trova il video player nel componente padre
-        const videoPlayer = document.querySelector('video[x-ref=\"videoPlayer\"]');
-        if (videoPlayer) {
-            videoPlayer.currentTime = timestamp;
-            this.currentTime = timestamp;
-        }
-    }
+    duration: {{ $duration }}
 }" 
-     x-init="initTimeline()"
      class="snap-timeline position-relative">
     
     <!-- Progress Bar -->
@@ -36,7 +19,7 @@
         }"
              class="snap-marker position-absolute" 
              style="left: {{ ($snap->timestamp / ($duration ?: 1)) * 100 }}%"
-             x-on:click="seekToTimestamp(snapTimestamp)"
+             x-on:click="document.querySelector('video[x-ref=\\'videoPlayer\\']').currentTime = snapTimestamp; $wire.currentTime = snapTimestamp"
              x-on:mouseenter="showTooltip = true"
              x-on:mouseleave="showTooltip = false">
             <div class="snap-indicator bg-success rounded-circle d-flex align-items-center justify-content-center"
